8d484cd318ff3e940945d822c5f42f3c5ec761d8
Rodrigo Hernandez <kwizatz@aeongames.com>
2016-09-14 17:59:36 -0600

    Recovery image compilation adjustments.

--- device_coreprimelte.mk
+++ device_coreprimelte.mk
@@ -22,3 +22,8 @@ $(call inherit-product, build/target/product/full.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_coreprimelte
 PRODUCT_DEVICE := coreprimelte
+
+# Build characteristics setting 
+PRODUCT_CHARACTERISTICS := phone
+PRODUCT_AAPT_CONFIG := mdpi hdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
